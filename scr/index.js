6c7da283aed1ea37821bea9b33cb9dff912e62ad
Ikarus567 <ikinhoprogamacao21@outlook.com>
2026-02-26 20:35:43 -0300

    Enhance comments in index.js for clarity
    Added comments to clarify the purpose of the event listener.

--- scr/index.js
+++ scr/index.js
@@ -1,3 +1,5 @@
+// Quando o HTML terminar de carregar ela vai executa essa função
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Landing page carregada!");
+    console.log("Landing page carregada!");// Mostra essa mensagem no console do navegador (serve só pra avisar que a página carregou)
+
 });
